5ecd2519df58537447ab40c63577e9aa3006657e
Lin1ux <liniuxpl2@gmail.com>
2024-11-19 22:29:30 +0100

    human scritpt init

---
+++ scenes/level_select.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_exit_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+
+
+func _on_start_game_2_pressed() -> void:
+	pass # Replace with function body.
