f3fb155eb9bde2decc03e1b37a309627110d3a11
Lin1ux <liniuxpl2@gmail.com>
2024-11-21 00:43:11 +0100

    win lose panel animations

--- interface/ui.gd
+++ interface/ui.gd
@@ -2,6 +2,7 @@ extends Control
 
 @export var win_panel : Control
 @export var lose_panel : Control
+@export var anim : AnimationPlayer
 
 func _ready():
 	win_panel.visible = false
@@ -10,8 +11,10 @@ func _ready():
 	DeathManager.connect("not_all_dead",lose)
 
 func win():
+	anim.play("show_win")
 	win_panel.visible = true
 	
 func lose():
+	anim.play("show_lose")
 	lose_panel.visible = true
 	
