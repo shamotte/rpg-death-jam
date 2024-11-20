4ff272488cd18f3d48a132d56a4da98475eb9434
Lin1ux <liniuxpl2@gmail.com>
2024-11-21 00:57:32 +0100

    more comfortable paths in UI

--- interface/panel.gd
+++ interface/panel.gd
@@ -1,14 +1,20 @@
 extends Panel
 
-@export var next_level_path : String
+@export var next_level : String
 
 func _ready():
-	if next_level_path == "":
-		next_level_path = "res://scenes/main_menu.tscn"
+	if next_level == "":
+		next_level = "res://scenes/main_menu.tscn"
+		
+func set_next_level(new_level : String):
+	if new_level == "":
+		new_level = "res://scenes/main_menu.tscn"
+		return
+	next_level = "res://scenes/level" + new_level + ".tscn"
 
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 
 func _on_next_level_pressed() -> void:
-	get_tree().change_scene_to_file(next_level_path)
+	get_tree().change_scene_to_file(next_level)
--- interface/ui.gd
+++ interface/ui.gd
@@ -1,10 +1,16 @@
 extends Control
 
+@export_group("Levels")
+@export var current_level : String
+@export var next_level : String
+@export_group("Nodes Inside")
 @export var win_panel : Control
 @export var lose_panel : Control
 @export var anim : AnimationPlayer
 
 func _ready():
+	win_panel.set_next_level(next_level)
+	lose_panel.set_current_level(current_level)
 	win_panel.visible = false
 	lose_panel.visible = false
 	DeathManager.connect("all_dead",win)
